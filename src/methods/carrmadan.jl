de4bc11616247c19b7331d4bdae2f2d9fda49399
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-07-28 18:46:20 +0200

    models implemented

--- src/methods/carrmadan.jl
+++ src/methods/carrmadan.jl
@@ -1,3 +1,7 @@
+using FinancialMonteCarlo
+
+import FinancialMonteCarlo.pricer;
+
 """
 Struct for Carr Madan Method
 
@@ -20,22 +24,69 @@ mutable struct CarrMadanMethod{num<:Number}#<:ItoProcess
     end
 end
 
-function CharactheristicExponent(mcProcess::FinancialMonteCarlo.MertonProcess)
 
-σ=mcProcess.σ;
-sigma1=mcProcess.σJ
-mu1=mcProcess.μJ
-lam=mcProcess.λ
+function CharactheristicExponent(mcProcess::FinancialMonteCarlo.BlackScholesProcess)
 
-CharExp(u::Number)::Number=-σ*σ*u*u*0.5+lam*(exp(-sigma1*sigma1*u*u*0.5+1im*mu1*u)-1);
+	σ=mcProcess.σ;
 
-return CharExp;
+	CharExp(v::Number)::Number=0.5*v*σ.*σ*(1im-v);
+
+	return CharExp;
 
 end
 
+function CharactheristicExponent(mcProcess::FinancialMonteCarlo.MertonProcess)
 
+	σ=mcProcess.σ;
+	sigma1=mcProcess.σJ
+	mu1=mcProcess.μJ
+	lam=mcProcess.λ
 
-using Interpolations;
+	CharExp(u::Number)::Number=-σ*σ*u*u*0.5+lam*(exp(-sigma1*sigma1*u*u*0.5+1im*mu1*u)-1);
+
+	return CharExp;
+
+end
+
+function CharactheristicExponent(mcProcess::FinancialMonteCarlo.KouProcess)
+
+	σ=mcProcess.σ;
+	lamp=mcProcess.λp
+	lamm=mcProcess.λm
+	p=mcProcess.p
+	lam=mcProcess.λ
+
+	CharExp(u::Number)::Number=-σ*σ*u*u/2.0+1im*u*p*(lam/(lamp-1im*u)-(1-lam)/(lamm+1im*u));
+
+	return CharExp;
+
+end
+
+function CharactheristicExponent(mcProcess::FinancialMonteCarlo.VarianceGammaProcess)
+
+	σ=mcProcess.σ;
+	theta1=mcProcess.θ
+	k1=mcProcess.κ
+
+	CharExp(u::Number)::Number=-1/k1*log(1+u*u*σ*σ*k1/2.0-1im*theta1*k1*u);
+
+	return CharExp;
+
+end
+
+function CharactheristicExponent(mcProcess::FinancialMonteCarlo.NormalInverseGaussianProcess)
+
+	σ=mcProcess.σ;
+	theta1=mcProcess.θ
+	k1=mcProcess.κ
+
+	CharExp(v::Number)=(1-1*sqrt(1.0 + ((v^2)*(σ*σ)-2*1im*theta1*v)*k1))/k1;
+
+	return CharExp;
+
+end
+
+using Interpolations,FFTW;
 """
 Pricing European Options through Fast Fourier Transform Method (Carr Madan)
 
--- src/methods/main.jl
+++ src/methods/main.jl
@@ -1,3 +1,7 @@
+using FinancialMonteCarlo,FinancialToolbox
+include("carrmadan.jl")
+
+
 A=400.0;
 N=12;
 
@@ -6,7 +10,7 @@ S0=100.0;
 K=100.0;
 r=0.02;
 T=1.0;
-d=0.01;
+d=0.0;
 sigma=0.2; 
 lam=5.0; 
 mu1=0.03; 
@@ -14,7 +18,13 @@ sigma1=0.02;
 spotData1=equitySpotData(S0,r,d);
 
 Model=MertonProcess(sigma,lam,mu1,sigma1);
+Model=BlackScholesProcess(sigma);
 
 EUData=EuropeanOption(T,K)
+Nsim=10000;
+Nstep=30;
+mc=MonteCarloConfiguration(Nsim,Nstep);
 
-pricer(method,Model,spotData1,EUData)
+@show pricer(method,Model,spotData1,EUData)
+@show pricer(Model,spotData1,mc,EUData);
+@show blsprice(S0,K,r,T,sigma,d)
