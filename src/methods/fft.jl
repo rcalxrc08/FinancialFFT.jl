c1d2ce03f4305dfa88cdca3ad78529c937806779
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-07-29 22:59:07 +0200

    working

--- src/methods/fft.jl
+++ src/methods/fft.jl
@@ -1,14 +1,14 @@
 using DualNumbers
-import Base.fft;
+import FFTW.fft;
 function fft(x::Array{DualComplex256})
 	N1=length(x);
-	Xcomplex=value.(x);
+	Xcomplex=DualNumbers.value.(x);
 	Xder=epsilon.(x);
 	fft!(Xcomplex);
 	derY=zeros(N1);
 	for i in 1:N1
 		if !isapprox(real(Xder[i]),0.0)&&!isapprox(imag(Xder[i]),0.0)
-			Xup=zeros(Complex128,N1);
+			Xup=zeros(ComplexF64,N1);
 			Xup[i]=1.0;
 			fft!(Xup);
 			derY+=Xup.*Xder[i];
@@ -16,6 +16,6 @@ function fft(x::Array{DualComplex256})
 	end
 	#Yout=[DualComplex256(Xcomplex[i],derY[i]) for i in 1:N1];
 	Yout=[DualComplex256(real1,epsilon1) for (real1,epsilon1) in zip(Xcomplex,derY)];
-	
+
 	return Yout;
 end
--- src/methods/main.jl
+++ src/methods/main.jl
@@ -6,14 +6,14 @@ A=400.0;
 N=12;
 
 method=CarrMadanMethod(A,N);
-S0=dual(100.0,1.0);
+S0=100.0;
 K=100.0;
 r=0.02;
 T=1.0;
 d=0.01;
-sigma=0.2; 
-lam=15.0; 
-mu1=0.03; 
+sigma=dual(0.2,1.0);
+lam=15.0;
+mu1=0.03;
 sigma1=0.02;
 spotData1=equitySpotData(S0,r,d);
 
@@ -25,6 +25,6 @@ Nsim=10000;
 Nstep=30;
 mc=MonteCarloConfiguration(Nsim,Nstep);
 
-@show pricer(Model,spotData1,method,EUData);
-@show pricer(Model,spotData1,mc,EUData);
-typeof(Model)<:BlackScholesProcess ? @show(blsprice(S0,K,r,T,sigma,d)) : nothing ;
+@time @show pricer(Model,spotData1,method,EUData);
+@time @show pricer(Model,spotData1,mc,EUData);
+@time typeof(Model)<:BlackScholesProcess ? @show(blsprice(S0,K,r,T,sigma,d)) : nothing ;
