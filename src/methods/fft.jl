eb5dc6b47e28b1df039f3e151a26cfad6bcc939a
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-09-07 22:36:30 +0200

    fft

--- src/methods/fft.jl
+++ src/methods/fft.jl
@@ -1,21 +1,11 @@
 using DualNumbers,FFTW
 import FFTW.fft;
 function fft(x::Array{DualComplex256})
-	N1=length(x);
 	Xcomplex=DualNumbers.value.(x);
 	Xder=epsilon.(x);
 	fft!(Xcomplex);
-	derY=zeros(N1);
-	for i in 1:N1
-		if !isapprox(real(Xder[i]),0.0)&&!isapprox(imag(Xder[i]),0.0)
-			Xup=zeros(ComplexF64,N1);
-			Xup[i]=1.0;
-			fft!(Xup);
-			derY+=Xup.*Xder[i];
-		end
-	end
-	#Yout=[DualComplex256(Xcomplex[i],derY[i]) for i in 1:N1];
-	Yout=[DualComplex256(real1,epsilon1) for (real1,epsilon1) in zip(Xcomplex,derY)];
+	fft!(Xder)
+	Yout=[DualComplex256(real1,epsilon1) for (real1,epsilon1) in zip(Xcomplex,Xder)];
 
 	return Yout;
 end
