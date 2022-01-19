9b31d7a12503981c3d1033fc4756fac993974e3c
Nicola <nicola.scaramuzzino@yandex.com>
2022-01-19 13:43:15 +0100

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
-using Documenter, FinancialFFT, Literate
-EXAMPLE = joinpath(@__DIR__, "..", "examples", "getting_started.jl")
-OUTPUT = joinpath(@__DIR__, "src")
-Literate.markdown(EXAMPLE, OUTPUT; documenter = true)
-makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "FinancialFFT.jl", modules = [FinancialFFT], pages = ["index.md", "getting_started.md", "types.md", "stochproc.md", "parallel_vr.md", "payoffs.md", "metrics.md", "multivariate.md", "intdiffeq.md", "extends.md"])
+using Documenter, FinancialFFT, Literate, FinancialMonteCarlo
+# EXAMPLE = joinpath(@__DIR__, "..", "examples", "getting_started.jl")
+# OUTPUT = joinpath(@__DIR__, "src")
+# Literate.markdown(EXAMPLE, OUTPUT; documenter = true)
+# makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "FinancialFFT.jl", modules = [FinancialFFT], pages = ["index.md", "getting_started.md", "types.md", "stochproc.md", "parallel_vr.md", "payoffs.md", "metrics.md", "multivariate.md", "intdiffeq.md", "extends.md"])
+makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "FinancialFFT.jl", modules = [FinancialFFT], pages = ["index.md", "types.md", "stochproc.md", "parallel_vr.md", "payoffs.md", "metrics.md", "multivariate.md", "intdiffeq.md", "extends.md"])
 get(ENV, "CI", nothing) == "true" ? deploydocs(repo = "https://gitlab.com/rcalxrc08/FinancialFFT.jl.git") : nothing
