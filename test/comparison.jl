29077fadaf16c535ac086188456c7f69b61a6cd3
Nicola <nicola.scaramuzzino@yandex.com>
2022-01-19 18:07:04 +0100

    proper tests

--- test/comparison.jl
+++ test/comparison.jl
@@ -1,41 +1,39 @@
-using FinancialToolbox, DualNumbers, HyperDualNumbers, FinancialFFT, FinancialMonteCarlo
+using FinancialToolbox, Test, DualNumbers, HyperDualNumbers, FinancialFFT, FinancialMonteCarlo
 
 A = 600.0;
 N = 18;
 
 method_cm = CarrMadanMethod(A, N);
 S0 = 100.0;
-# S0 = dual(100.0, 1.0);
 K = 100.0;
-# K = dual(100.0, 1.0);
-# r = hyper(0.02, 1.0, 1.0, 0.0);
-# r = dual(0.02, 1.0);
 r = 0.02;
 T = 1.0;
-# T = dual(1.0, 1.0);
-# d = hyper(0.0, 0.0, 1.0, 0.0);
 d = 0.01
-# d = 0.0;
 sigma = dual(0.2, 1.0);
-# sigma = hyper(0.2, 1.0, 1.0, 0.0);
-# sigma = 0.2;
 lam = 15.0;
 mu1 = 0.03;
 sigma1 = 0.02;
 zero_rate = ZeroRate(r);
 
-# Model = MertonProcess(sigma, lam, mu1, sigma1, Underlying(S0, d));
 Model = BlackScholesProcess(sigma, Underlying(S0, d));
 
 EUData = EuropeanOption(T, K)
 Nsim = 100000;
 Nstep = 30;
 mc = MonteCarloConfiguration(Nsim, Nstep, FinancialMonteCarlo.AntitheticMC());
+analytic_result = blsprice(S0, K, r, T, sigma, d);
+function test_dual(x, y, toll)
+    @test abs(x.value - y.value) < toll
+    @test abs(x.epsilon - y.epsilon) < toll
+    nothing
+end
 
-@time @show pricer(Model, zero_rate, method_cm, EUData);
+result_cm = pricer(Model, zero_rate, method_cm, EUData);
 method_lewis = LewisMethod(700.0, 200000);
-@time @show pricer(Model, zero_rate, method_lewis, EUData);
-@time @show pricer(Model, zero_rate, mc, EUData);
-typeof(Model) <: BlackScholesProcess ? @time(@show(blsprice(S0, K, r, T, sigma, d))) : nothing;
+result_lewis = pricer(Model, zero_rate, method_lewis, EUData);
 method_cm_lewis = CarrMadanLewisMethod(A, N);
-@time @show pricer(Model, zero_rate, method_cm_lewis, EUData);
+result_cm_lewis = pricer(Model, zero_rate, method_cm_lewis, EUData);
+toll = 1e-2;
+test_dual(analytic_result, result_cm, toll)
+test_dual(analytic_result, result_lewis, toll)
+test_dual(analytic_result, result_cm_lewis, toll)
