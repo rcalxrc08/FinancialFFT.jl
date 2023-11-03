e520a4a3e5c2441bcbc74f51284a970084fa41f4
Nicola <nicola.scaramuzzino@yandex.com>
2023-11-03 11:24:16 +0100

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,5 @@ using Documenter, FinancialFFT, Literate, FinancialMonteCarlo
 # OUTPUT = joinpath(@__DIR__, "src")
 # Literate.markdown(EXAMPLE, OUTPUT; documenter = true)
 # makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "FinancialFFT.jl", modules = [FinancialFFT], pages = ["index.md", "getting_started.md", "types.md", "stochproc.md", "parallel_vr.md", "payoffs.md", "metrics.md", "multivariate.md", "intdiffeq.md", "extends.md"])
-makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "FinancialFFT.jl", modules = [FinancialFFT], pages = ["index.md", "types.md", "stochproc.md", "metrics.md"])
+makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"], repolink = "https://gitlab.com/rcalxrc08/FinancialFFT.jl"), repo = "https://gitlab.com/rcalxrc08/FinancialFFT.jl.git", sitename = "FinancialFFT.jl", modules = [FinancialFFT], pages = ["index.md", "types.md", "stochproc.md", "metrics.md"])
 get(ENV, "CI", nothing) == "true" ? deploydocs(repo = "https://gitlab.com/rcalxrc08/FinancialFFT.jl.git") : nothing
