eaa0d97f2b5d6098d8ce4296aecb57ffde352c00
Nicola <nicola.scaramuzzino@yandex.com>
2023-11-04 13:37:31 +0100

    rename

--- src/methods/utils.jl
+++ src/methods/utils.jl
@@ -15,18 +15,5 @@ function call_to_put(C, _, df, opt::BinaryEuropeanOption)
     return res
 end
 
-function midpoint_definite_integral(f, xmin, xmax, N)
-    x = range(xmin, length = N, stop = xmax)
-    dx = (xmax - xmin) / (N - 1)
-    sum_ = zero(typeof(f(xmin))) * dx * 0
-    weights_ = @. 3 + (-1)^((0:(N-1)) + 1)
-    @views weights_[1] = 1
-    @views weights_[N] = 1
-    for (x_, w) in zip(x, weights_)
-        x_ != 0 && !isnan(f(x_)) && (sum_ += f(x_) * w * dx / 3)
-    end
-    return sum_
-end
-
-include("AlternateVectors.jl")
+include("alternate_vectors.jl")
 include("alternate_padded.jl")
