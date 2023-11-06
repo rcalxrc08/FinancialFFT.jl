2eabc7fd0c2c1a692a1147421ba0c31178cdfdfd
Nicola <nicola.scaramuzzino@yandex.com>
2023-11-06 12:13:03 +0100

    integral not used

--- src/deps/cuda_dependencies.jl
+++ src/deps/cuda_dependencies.jl
@@ -1,15 +1,5 @@
 using .CUDA
 
-function midpoint_definite_integral_cu(f, xmin, xmax, N)
-    x = cu(collect(range(xmin, length = N, stop = xmax)))
-    dx = (xmax - xmin) / (N - 1)
-    # sum_ = zero(typeof(f(xmin))) * dx * 0
-    # for x_ in x
-    #     x_ != 0 && !isnan(f(x_)) && (sum_ += f(x_) * dx)
-    # end
-    return sum(f.(x) .* dx)
-end
-
 include("../methods/cuda/carrmadan.jl")
 function adapt_array(x, ::FinancialMonteCarlo.AbstractCudaMode)
     return cu(x)
