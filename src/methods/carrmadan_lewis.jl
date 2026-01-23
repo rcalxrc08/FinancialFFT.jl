d1863b980ab923b9871acf8c21f43383a382fb91
Nicola <nicola.scaramuzzino@yandex.com>
2026-01-23 19:59:39 +0100

    proper interface for CarrMadanLewis

--- src/methods/carrmadan_lewis.jl
+++ src/methods/carrmadan_lewis.jl
@@ -45,3 +45,29 @@ function pricer(mcProcess::FinancialMonteCarlo.BaseProcess, StrikeVec::Array{U, 
     prices = @. S0 * exp(dT) - exp(-rT) / pi * dx / 3 * sqrt(S0 * StrikeVec) * spline_cub(log(StrikeVec / S0))
     return prices
 end
+
+function pricer(mcProcess::FinancialMonteCarlo.BaseProcess, zero_rate::FinancialMonteCarlo.AbstractZeroRateCurve, method::CarrMadanLewisMethod, opt, mode::FinancialMonteCarlo.BaseMode = FinancialMonteCarlo.SerialMode())
+    Npow = method.Npow
+    N = ChainRulesCore.@ignore_derivatives 2^Npow
+    S0 = mcProcess.underlying.S0
+    A = method.A
+    T = opt.T
+    dT = -FinancialMonteCarlo.integral(FinancialMonteCarlo.dividend(mcProcess), T)
+    rT = FinancialMonteCarlo.integral(zero_rate.r, T)
+    correction = rT + dT - FinancialFFT.characteristic_exponent_i(1, mcProcess) * T
+    idx = ChainRulesCore.@ignore_derivatives 0:(N-1)
+    one_minus_one = ChainRulesCore.@ignore_derivatives AlternateVector(Int8(1), Int8(-1), N)
+    weights_simpson = ChainRulesCore.@ignore_derivatives AlternatePaddedVector(Int8(1), Int8(4), Int8(2), Int8(1), N)
+    support_array = ChainRulesCore.@ignore_derivatives @. one_minus_one * weights_simpson
+    dx = A / N
+    x = ChainRulesCore.@ignore_derivatives adapt_array(collect(idx * dx), mode)
+    x_im = @. 1 // 2 + x * im
+    Y = fft(@. support_array * exp(correction * x_im + FinancialFFT.characteristic_exponent_i(x_im, mcProcess) * T) / abs2(x_im))
+    integral_value = @. real_mod(Y)
+    pi_over_A = pi / A
+    ks = ChainRulesCore.@ignore_derivatives pi_over_A * (-N .+ 2 * idx)
+    spline_cub = ChainRulesCore.@ignore_derivatives adapt_itp(CubicSplineInterpolation(ks, integral_value), mode)
+    StrikeVec = opt.K
+    prices = @. S0 * exp(dT) - exp(-rT) / pi * dx / 3 * sqrt(S0 * StrikeVec) * $spline_cub(log(StrikeVec / S0))
+    return call_to_put(prices, S0 * exp(dT), exp(-rT), opt)
+end
